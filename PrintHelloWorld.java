e0da5988a55ad33f6a033402d83358154b9aeb63
KaiID175 <Liu1901754516@163.com>
2025-03-27 22:00:04 +0800

    Update PrintHelloWorld.java

--- PrintHelloWorld.java
+++ PrintHelloWorld.java
@@ -5,7 +5,7 @@ public class PrintHelloWorld{
 	public static void print(int n){
 		for(int i = 1;i <= n;i++){
 			System.out.println("Hello World!");
-			Systm.out.println("Hello Git----at the GitHub");
+			System.out.println("Hello Git----at the GitHub");
 		}
 	}
 }
