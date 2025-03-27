10de6a2f35cee2554eba4c89685ebce526e9b015
ZhenKai <190175415@qq.com>
2025-03-27 21:41:01 +0800

    firstCommit

---
+++ PrintHelloWorld.java
@@ -0,0 +1,10 @@
+public class PrintHelloWorld{
+	public static void main(String[] args){
+        	print(5);
+    }
+	public static void print(int n){
+		for(int i = 1;i <= n;i++){
+			System.out.println("Hello World!");
+		}
+	}
+}
