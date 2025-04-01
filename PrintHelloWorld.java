70250ffdf32b76f03d51695860f7c78978b45c9c
KaiID175 <Liu1901754516@163.com>
2025-04-01 10:31:19 +0800

    GitFIrstCommit PrintHelloWorld.java

--- PrintHelloWorld.java
+++ PrintHelloWorld.java
@@ -1,6 +1,8 @@
 public class PrintHelloWorld{
 	public static void main(String[] args){
         	print(5);
+		System.out.println("-----hot-fix------------------");
+		System.out.println("Today is Apr.1th");
     }
 	public static void print(int n){
 		for(int i = 1;i <= n;i++){
